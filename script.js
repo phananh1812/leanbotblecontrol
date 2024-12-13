d2831a3d83c097a5a687ca57b708bf849472b90a
PRO-8\Pro 8 <phananh1812@gmail.com>
2024-12-13 09:51:14 +0700

    1

--- script.js
+++ script.js
@@ -265,62 +265,69 @@ function moveturnRight(angle) {
 }
 
 annyang.addCommands({
-    'light on' : ledOn,
-    'Light on.' : ledOn,
-
-    'light off' : ledOff,
-    'Light off.' : ledOff,
-
-    'open' : gripperOpen,
-    'Open.' : gripperOpen,
-
-    'close': gripperClose,
-    'Close.': gripperClose,
-
-    'turn left':Left,
-    'Turn left.':Left,
-
-    'turn right':Right,
-    'Turn right.':Right,
-
-    'forward': Forward,
-    'Forward.': Forward,
-
-    'backward': Backward,
-    'Backward.': Backward,
-
-    'sound on' : hornOn,
-    'Sound on.' : hornOn,
-
-    'sound off' : hornOff,
-    'Sound off.' : hornOff,
-
-    'stop': Stop,
-    'Stop.': Stop,
-    
-    // 'close the door': CloseDoor,
-    // 'open the door': OpenDoor,
-    // 'go to field one': Feild1,
-    // 'go to field two': Feild2,
-    // 'go to field three': Feild3,
-    // 'go to field four': Feild4,
-    // 'go to field 1': Feild1,
-    // 'go to field 2': Feild2,
-    // 'go to field 3': Feild3,
-    // 'go to field 4': Feild4,
-    // 'forward *distance cm': function(distance) {
-    //     moveForward(parseInt(distance));
-    // },
-    // 'backward *distance cm': function(distance) {
-    //     moveBackward(parseInt(distance));
-    // },
-    // 'turn left *angle degrees': function(angle) {
-    //     moveturnLeft(parseInt(angle));
-    // },
-    // 'turn right *angle degrees': function(angle) {
-    //     moveturnRight(parseInt(angle));
-    // }
+    '*text': function(text) {
+      const lowercaseText = text.toLowerCase();                                                 // Convert to lowercase for comparison
+      console.log('User said:', lowercaseText);
+      
+      // Loop through the commands and check if the string starts with any of the commands
+      for (const command in commands) {
+        if (lowercaseText.startsWith(command)) {
+          console.log(`Matched command: "${command}" -> Calling corresponding function`);
+          commands[command]();                                                                  // Call the corresponding function
+          break;                                                                                // Exit the loop after finding a matching command
+        }
+      }
+    }
 });
+
+const commands = {
+    'light on': ledOn,
+    'light off': ledOff,
+    'open': gripperOpen,
+    'close': gripperClose,
+    'turn left': Left,
+    'turn right': Right,
+    'forward': Forward,
+    'backward': Backward,
+    'sound on': hornOn,
+    'sound off': hornOff,
+    'stop': Stop
+  };
+
+// annyang.addCommands({
+//     'light on' : ledOn,
+//     'Light on.' : ledOn,
+
+//     'light off' : ledOff,
+//     'Light off.' : ledOff,
+
+//     'open' : gripperOpen,
+//     'Open.' : gripperOpen,
+
+//     'close': gripperClose,
+//     'Close.': gripperClose,
+
+//     'turn left':Left,
+//     'Turn left.':Left,
+
+//     'turn right':Right,
+//     'Turn right.':Right,
+
+//     'forward': Forward,
+//     'Forward.': Forward,
+
+//     'backward': Backward,
+//     'Backward.': Backward,
+
+//     'sound on' : hornOn,
+//     'Sound on.' : hornOn,
+
+//     'sound off' : hornOff,
+//     'Sound off.' : hornOff,
+
+//     'stop': Stop,
+//     'Stop.': Stop,
+// });
 
 annyang.addCallback('result', function(phrases) {
     document.getElementById('spokenCommand').innerHTML = 'You said: ' + phrases[0];
